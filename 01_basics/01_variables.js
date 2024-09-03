a55fd172ed2e2eeec8c1cb1fe0d0aad7ffec54c3
Sohaib-Raheem <shanineymar07@gmail.com>
2024-09-03 08:03:13 +0000

    story of let , cont and var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId=16052004
+let accountEmail="chaicode@gmail.com"
+var accountPassword="12345"
+accountCity="Lahore"
+
+//accountId=2 // const will not be changed
+//console.log(accountId);
+
+accountEmail="coder@gmail.com"
+accountPassword="212121"
+accountCity="Karachi"
+
+//console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+let accountState; // UNDEFINED will be printed
+console.log(accountState);
+
+/* prefer not to use VAR
+because of issue in block scope and functional scope
+*/
