af0e09047708fe201d7202782d68b8a12b110776
Sohaib-Raheem <shanineymar07@gmail.com>
2024-09-03 13:24:09 +0000

    datatypes and ECMA standards

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using node.js not browser
+
+console.log(3
+    +3) //code readability should be high , try to write neat and clean code
+
+console.log("sohaib")
+
+let name="sohaib"
+let age=20
+let isLoggedIn=false
+let state;
+
+//number => 2 to power 53
+//big int
+// string =>""
+// Boolean => true/false
+// null => stand alone value
+// undefined =>
+// symbol => unique
+
+
+// OBJECT
+
+console.log(typeof "sohaib");
+console.log(typeof age);
+console.log(typeof null); // output=> object
+console.log(typeof undefined); // output=> undefined
