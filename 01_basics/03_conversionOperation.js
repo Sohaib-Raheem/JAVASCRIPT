076931f3ebd6d09011a391f360f3069e99083933
Sohaib-Raheem <shanineymar07@gmail.com>
2024-09-04 07:59:50 +0000

    Conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,9 +24,56 @@ let booleanIsLoggedIn= Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber= String (someNumber)
-console.log(stringNumber);
+//console.log(stringNumber);
 
 // now we will check that , it is converted in string or not
-console.log(typeof (stringNumber));
+//console.log(typeof (stringNumber));
 
 //output => string
+
+//************************** OPERATIONS *******************//
+
+let value= 3
+let negValue=-value
+//console.log(negValue);
+
+/* console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/2)
+console.log(2%2) */
+
+let str1= "hello"
+let str2= " sohaib"
+
+let str3= str1+str2
+//console.log(str3); // ouput => hello sohaib
+
+/*console.log(1+"2") // output => 12
+console.log("1"+2) // output => 12
+console.log(1+"2"+2) // output => 122
+console.log("1"+2+2) // output => 122
+console.log(1+2+"2") // output => 32
+console.log( (3+4) * 5 % 3 );
+
+console.log( +true ); // output => 1
+console.log( -"" );   // output => -0
+console.log( +false );// output => 0
+console.log( -true ); // output => -1 */
+
+let num, num2, num3
+num1 = mum2 = num3 = 2+2 // dont assign values in that way , code should be easy to read
+
+let gameCounter = 100
+//gameCounter++; output => 101
+++gameCounter; // output => 101
+console.log(gameCounter);
+
+
+
+
+
+
+
+
