cace868304cb62ca1de5f785da979bface78fff5
Sohaib-Raheem <shanineymar07@gmail.com>
2024-09-04 06:15:33 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+/*let score ="33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);*/
+
+
+//"33"=>33
+//"33abc"=> NaN
+//true =>1; false =>0
+
+let isLoggedIn= "sohaib"
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=> true ; 0=> false;
+// if we use empty string , so output will be false;  "" => false 
+// if we add some value in string, so output will be true; "sohaib" => true
+
+let someNumber = 33
+
+let stringNumber= String (someNumber)
+console.log(stringNumber);
+
+// now we will check that , it is converted in string or not
+console.log(typeof (stringNumber));
+
+//output => string
